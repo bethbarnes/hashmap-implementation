c1b1f48104f9b87af026ad2dab833dc511bb490d
Beth Barnes <bethcbarnes@outlook.com>
2018-07-09 16:33:58 -0400

    feat (hashMap): implement simple hashing function for testing purposes

--- hashMap.js
+++ hashMap.js
@@ -1,3 +1,22 @@
 //implement hashmap as array, hash function gives us array at which
 // to store data, use Linked List to deal with collisions.
 
+class HashMap {
+  constructor(){
+    this.hashLength = 50;
+    this.hashArr = new Array(50)
+  }
+
+  addToHash(val){
+    let hash = this.hash(val)
+    console.log('hash is: ', hash)
+  }
+
+//very simple hashing function, will replce
+  hash(val){
+    return val.charCodeAt(0)%this.hashLength
+  }
+
+}
+
+
