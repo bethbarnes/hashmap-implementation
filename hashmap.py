7ddb694de647ce65d06ac0e0f468c3a63bc6816b
Beth Barnes <bethcbarnes@outlook.com>
2018-07-17 21:20:30 -0400

    fix (All): changes syntax, refactors some code

--- hashmap.py
+++ hashmap.py
@@ -1,9 +1,65 @@
 #!/usr/bin/env python
 
-from linkedlist import number
+from linkedlist import Linked_List
 
+#---------SOLUTION----------#
+
+#initializing hash map
+class Hash_Map:
+  hash_length = 26
+  hash_arr = [None] * hash_length
+
+#simple hashing function
+  def hash(self, key):
+    if type(key) is str:
+      return ord(key[0]) % self.hash_length
+    elif type(key) is int:
+      return  ord(str(key)[0]) % self.hash_length
+
+#add key and value to hash map
+  def add_to_hash(self, key, val):
+    hash_num = self.hash(key)
+    if self.hash_arr[hash_num] is None :
+      self.hash_arr[hash_num] = Linked_List()
+    self.hash_arr[hash_num].add_LL_node(key, val)
+
+#find key in hash map and return value
+  def look_up(self, key):
+    hash_num = self.hash(key)
+    if self.hash_arr[hash_num] is None :
+      raise KeyError(f'key ({key}) not found')
+    else :
+      return self.hash_arr[hash_num].find_node(key)
+
+
+#--------TESTS--------#
 def main():
-    print('hello world', number )
+
+  print('test - initializes empty hash map with no keys or values')
+
+  inventory_hash = Hash_Map()
+
+  try:
+    inventory_hash.look_up('apple')
+  except KeyError as e:
+    assert e.args[0] == 'key (apple) not found'
+
+  print('test - adds key and value to hash map, can lookup keys')
+
+  inventory_hash.add_to_hash('apples', 30)
+  inventory_hash.add_to_hash('oranges', 50)
+
+  assert inventory_hash.look_up('apples') == 30
+  assert inventory_hash.look_up('oranges') == 50
+
+  print('test - handles collisions')
+
+  inventory_hash.add_to_hash('bananas', 25)
+  inventory_hash.add_to_hash('beans', 15)
+
+  assert inventory_hash.look_up('bananas') == 25
+  assert inventory_hash.look_up('beans') == 15
+
 
 if __name__ == '__main__':
-    main()
+  main()
--- linkedlist.py
+++ linkedlist.py
@@ -1,1 +1,38 @@
-number = 3
+#!/usr/bin/env python
+
+class Node:
+  def __init__(self, key=None, val=None):
+    self.next_node = None
+    self.key = key
+    self.val = val
+
+class Linked_List:
+  def __init__(self):
+    self.head = None
+
+  def add_LL_node(self, key, val):
+    new_node = Node(key, val)
+    self.head = new_node
+    if self.head is not None:
+      previous_head = self.head
+      new_node.next_node = previous_head
+
+  def find_node(self, key):
+    curr_node = self.head
+    while curr_node:
+      if curr_node.key == key:
+        return curr_node.val
+      curr_node = curr_node.next_node
+    raise KeyError('key not found')
+
+def main():
+  myLL = Linked_List()
+  myLL.add_LL_node('beth', 24)
+  myLL.add_LL_node('mom', 55)
+  myLL.add_LL_node('dad', 60)
+  # print(myLL.head.next.val)
+  # print(myLL.find_node('mom'))
+  # myLL.find_node('dad')
+
+if __name__ == '__main__':
+  main()
